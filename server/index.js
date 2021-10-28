7f60cf39f82a0c74aea9b6fa8c21b15d120d9fe5
Mike Ortiz <emike108@aim.com>
2021-10-28 11:16:52 -0700

    additional server initialization steps taken

--- server/index.js
+++ server/index.js
@@ -4,7 +4,14 @@ const app = express();
 
 const port = 3000;
 
-app.use(express.static(__dirname + '../client/dist'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
+app.use(express.static(__dirname + '/../client/dist'));
+
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// })
 
 
 app.listen(port, (err) => {
