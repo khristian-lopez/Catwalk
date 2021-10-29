ecdf444f81303f116a8b0b38e1f29086b10ab7a8
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-10-29 14:44:45 -0700

    remodified changes to only show my ratings folder and its components

--- client/src/components/relatedProducts/ActionButton.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react';
-
-const ActionButton = () => (
-  <span className='action-button'>
-    &#9734;
-  </span>
-)
-
-export default ActionButton;
--- client/src/components/relatedProducts/relatedProducts.jsx
+++ client/src/components/relatedProducts/relatedProducts.jsx
@@ -1,14 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import RelatedProductsList from './RelatedProductsList.jsx';
-import YourOutfitList from './YourOutfitList.jsx';
+import React from 'react';
 
 const RelatedProducts = () => (
-  <div id="related">
-    <h3> Related Products </h3>
-    <RelatedProductsList />
-    <h3> Your Outfit </h3>
-    <YourOutfitList />
-  </div>
+    <div> relatedProducts </div>
 )
 
 export default RelatedProducts;
