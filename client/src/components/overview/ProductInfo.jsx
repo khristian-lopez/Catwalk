be508f56e47616593d7904bfedfcc7c4476eff80
Mike Ortiz <emike108@aim.com>
2021-11-02 15:12:14 -0700

    product description text area initialized

--- client/src/components/overview/ProductInfo.jsx
+++ client/src/components/overview/ProductInfo.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 
 const ProductInfo = () => (
   <Col className='col2' xs={7}>
-    <h6>product Overview</h6>
+    <Form className="ov-textarea">
+        <Form.Group >
+            <Form.Label>Product Overview</Form.Label>
+            <Form.Control as="textarea" rows={6} plaintext readOnly defaultValue="This is an example description of the product selected"/>
+        </Form.Group>
+    </Form>
   </Col>
 );
 
