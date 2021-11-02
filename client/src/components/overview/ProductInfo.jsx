be91c3c2b711b1d90d91617ca4d299c9f5aa9726
Mike Ortiz <emike108@aim.com>
2021-11-02 15:31:56 -0700

    share icons initialized

--- client/src/components/overview/ProductInfo.jsx
+++ client/src/components/overview/ProductInfo.jsx
@@ -6,10 +6,10 @@ import Form from 'react-bootstrap/Form';
 
 const ProductInfo = () => (
   <Col className='col2' xs={7}>
-    <Form className="ov-textarea">
+    <Form>
         <Form.Group >
             <Form.Label>Product Overview</Form.Label>
-            <Form.Control as="textarea" rows={6} plaintext readOnly defaultValue="This is an example description of the product selected"/>
+            <Form.Control className="ov-textarea" as="textarea" rows={6} plaintext readOnly defaultValue="This is an example description of the product selected"/>
         </Form.Group>
     </Form>
   </Col>
