3f681cce093eb1f5b0c7ef6e4be7e80e9a4876b6
Phi Truong <phihtruong00@gmail.com>
2021-11-01 20:04:06 -0700

    Implemented basic carousel for related products list and outfit list

--- client/src/components/relatedProducts/RelatedProductsList.jsx
+++ client/src/components/relatedProducts/RelatedProductsList.jsx
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import RelatedProductCard from './RelatedProductCard.jsx';
 
 const RelatedProductsList = () => (
-  <div className="rel-products-list">
-    <RelatedProductCard />
-    <RelatedProductCard />
-    <RelatedProductCard />
-  </div>
+  <CarouselProvider
+    className="rel-products-list"
+    naturalSlideWidth={200}
+    naturalSlideHeight={300}
+    totalSlides={6}
+    visibleSlides={4}
+  >
+  {/* TODO: make totalSlides dynamic */}
+    <Slider>
+      <Slide index={0}><RelatedProductCard card={'related'}/></Slide>
+      <Slide index={1}><RelatedProductCard card={'related'}/></Slide>
+      <Slide index={2}><RelatedProductCard card={'related'}/></Slide>
+      <Slide index={3}><RelatedProductCard card={'related'}/></Slide>
+      <Slide index={4}><RelatedProductCard card={'related'}/></Slide>
+      <Slide index={5}><RelatedProductCard card={'related'}/></Slide>
+    </Slider>
+    <ButtonBack>{'<'}</ButtonBack>
+    <ButtonNext>{'>'}</ButtonNext>
+  </CarouselProvider>
 )
 
 export default RelatedProductsList;
