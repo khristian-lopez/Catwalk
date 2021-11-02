e93ebb95dc1f6af0b44560686f5fa446c0a530fa
Mike Ortiz <emike108@aim.com>
2021-11-02 14:40:47 -0700

    row1 skeleton initialized

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -2,11 +2,49 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Carousel from 'react-bootstrap/Carousel'
 
 const ImageGallery = () => (
   <Col className='col' xs={8}>
     image gallery
     <div>image thumbnails</div>
+    <Carousel variant="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="assets/product-image-placeholder-300x300.jpeg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="assets/product-image-placeholder-300x300.jpeg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="assets/product-image-placeholder-300x300.jpeg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   </Col>
 );
 
--- client/src/components/overview/StyleInfo.jsx
+++ client/src/components/overview/StyleInfo.jsx
@@ -5,24 +5,60 @@ import Row from 'react-bootstrap/Row';
 import StarRatings from '../StarRatings.jsx';
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import SplitButton from 'react-bootstrap/SplitButton';
 
 const StyleInfo = () => (
   <Col className='col1'>
-    purchase style options
+    {/* specific product info */}
     <div>
-      <StarRatings /> Read all XX reviews
+      <StarRatings />  <br /> Read all XX reviews
     </div>
     <div>
       <label>Category</label>
       <h3>Product Name</h3>
       <label>Price</label>
     </div>
-    <Container className="ov-main">
-      style slector/thumbnails
-      <Image src="assets/product-image-placeholder-300x300.jpeg" thumbnail roundedCircle fluid/>
+    {/* container for styles thumbnails */}
+    <Container className="ov-styles">
+      Style &gt; Selected Style <br />
+      <div className="ov-style-thumbnails">
+        <Image src="assets/product-image-placeholder-300x300.jpeg" thumbnail roundedCircle fluid width="75" height="75"/>
+        <Image src="assets/product-image-placeholder-300x300.jpeg" thumbnail roundedCircle fluid width="75" height="75"/>
+        <Image src="assets/product-image-placeholder-300x300.jpeg" thumbnail roundedCircle fluid width="75" height="75"/>
+        <br />
+        <Image src="assets/product-image-placeholder-300x300.jpeg" thumbnail roundedCircle fluid width="75" height="75"/>
+        <Image src="assets/product-image-placeholder-300x300.jpeg" thumbnail roundedCircle fluid width="75" height="75"/>
+        <Image src="assets/product-image-placeholder-300x300.jpeg" thumbnail roundedCircle fluid width="75" height="75"/>
+
+      </div>
     </Container>
-    <Container className="ov-main">
-    <Button variant="primary" size="sm">add to cart</Button>{' '}
+    <br />
+    {/* add to cart/checkout component */}
+    <Container className="ov-add-to-cart">
+      <div>
+        {/* select a size dropdown */}
+        <SplitButton size="sm" variant="secondary" title="Select a size">
+          <Dropdown.Item href="#/action-1">XS</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">S</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">M</Dropdown.Item>
+          <Dropdown.Item href="#/action-4">L</Dropdown.Item>
+          <Dropdown.Item href="#/action-4">XL</Dropdown.Item>
+        </SplitButton>
+        {/* select a quatity dropdown */}
+        <SplitButton size="sm" variant="secondary" title="Quantity">
+          <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+          <Dropdown.Item href="#/action-4">4</Dropdown.Item>
+          <Dropdown.Item href="#/action-4">5</Dropdown.Item>
+        </SplitButton>
+      </div>
+      {/* checkout button */}
+      <Button variant="primary" size="sm">add to cart</Button>{' '}
+      {/* favorite/star icon?? */}
+
 
     </Container>
 
