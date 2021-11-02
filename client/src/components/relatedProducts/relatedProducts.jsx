e08bcfa3b7cee86d8c4aa6e46764d3a79b708fe3
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-11-02 11:36:17 -0700

    modified relatedProducts component to match main repo

--- client/src/components/relatedProducts/relatedProducts.jsx
+++ client/src/components/relatedProducts/relatedProducts.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import RelatedProductsList from './RelatedProductsList.jsx';
+import YourOutfitList from './YourOutfitList.jsx';
 
 const RelatedProducts = () => (
-    <div> relatedProducts </div>
+  <div id="related">
+    <h3> Related Products </h3>
+    <RelatedProductsList />
+    <h3> Your Outfit </h3>
+    <YourOutfitList />
+  </div>
 )
 
 export default RelatedProducts;
