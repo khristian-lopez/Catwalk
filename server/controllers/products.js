56fdd983ed4f8b20cfc55a9d5132fbbc0198c1b4
Phi Truong <phihtruong00@gmail.com>
2021-11-03 13:16:46 -0700

    Removed unnecessary backticks, added comments for better human readability of nested promises

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -17,22 +17,29 @@ module.exports = {
 
     let config = {
       headers: {
-        'Authorization': `${TOKEN}`
+        'Authorization': TOKEN
       }
     }
 
+    // retrieve all productIds related to current productId
     axios.get(`${API_URL}/products/${productId}/related`, config)
       .then(results => {
+        // Promise.all is taking an array of API calls with axios
+        // for each retrieved productId
         Promise.all(
+          // map over each retrieved productId and make an API call
+          // to retrieve product data
           results.data.map(id => {
             return axios.get(`${API_URL}/products/${id}`, config)
               .then(results => results.data)
               .catch(err => console.log('failed to retrieve a product: ', err));
           })
         )
+        // Promise.all waits until all API calls resolve then returns
+        // an array of the resolved values
           .then(results => {
-            console.log('retrieved all related products from api')
-            res.send(results)
+            console.log('retrieved all related products from api');
+            res.status(201).send(results);
           })
           .catch(err => console.log('failed to retrieve products: ', err));
       })
