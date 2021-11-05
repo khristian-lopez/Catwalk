d93bfe16f1d37131831eaf392ba0ae606a678c80
Phi Truong <phihtruong00@gmail.com>
2021-11-04 19:19:19 -0700

    Modified App state to only contain reviewMetadata;

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,23 +9,25 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentProductId: 42366,
-      reviewMetadata: []
+      reviewMetadata: { product_id: 42366 }
     };
 
     this.handleChangeProduct = this.handleChangeProduct.bind(this);
+    this.getReviewMetadata = this.getReviewMetadata.bind(this);
   }
 
   componentDidMount() {
-    axios.get(`reviews/meta/${this.state.currentProductId}`)
+    this.getReviewMetadata();
+  }
+
+  getReviewMetadata() {
+    axios.get(`reviews/meta/${this.state.reviewMetadata.product_id}`)
       .then(results => this.setState({ reviewMetadata: results.data }))
       .catch(err => console.error('failed to retrieve review metadata: ', err))
   }
 
   handleChangeProduct(productId) {
-    this.setState({
-      currentProductId: productId
-    });
+    this.getReviewMetadata();
   }
 
   render () {
@@ -33,11 +35,11 @@ class App extends React.Component {
       <div>
         <h1>Project Catwalk</h1>
         <div>
-          <Overview currentProduct={this.state.currentProductId}/>
+          <Overview currentProduct={this.state.reviewMetadata}/>
         </div>
-        <RelatedProducts currentProduct={this.state.currentProductId}/>
-        <Qa currentProduct={this.state.currentProductId}/>
-        <RatingsAndReviews currentProduct={this.state.currentProductId}/>
+        <RelatedProducts currentProduct={this.state.reviewMetadata} handleChangeProduct={this.handleChangeProduct}/>
+        <Qa currentProduct={this.state.reviewMetadata}/>
+        <RatingsAndReviews currentProduct={this.state.reviewMetadata}/>
       </div>
     )
   }
