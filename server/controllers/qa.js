c0007703a6b53bc3b0e61d87ae68ab637f67cd93
Khristian Lopez <khristianlopez@Khristians-MacBook-Air.local>
2021-11-05 11:26:37 -0700

    Implemented basic func for marking question as helpful

--- server/controllers/qa.js
+++ server/controllers/qa.js
@@ -2,16 +2,16 @@ const TOKEN = require('../../config.js').TOKEN;
 const axios = require('axios');
 const API_URL = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax`
 
+
+let config = {
+  headers: {
+    'Authorization': TOKEN
+  }
+}
+
 module.exports = {
   getAllQuestions: (req, res) => {
-    let config = {
-      headers: {
-        'Authorization': TOKEN
-      }
-    }
-
     var product_id = req.params.product_id
-
     axios.get(`${API_URL}/qa/questions?product_id=${product_id}`, config)
       .then(results => {
         console.log(`All questions for product ${product_id} have been retrieved.`)
@@ -24,14 +24,7 @@ module.exports = {
   },
 
   getAllAnswers: (req, res) => {
-    let config = {
-      headers: {
-        'Authorization': TOKEN
-      }
-    }
-
     var question_id = req.params.question_id
-
     axios.get(`${API_URL}/qa/questions/${question_id}/answers`, config)
     .then(results => {
       console.log(`All answers for question ${question_id} have been retrieved.`)
@@ -43,16 +36,8 @@ module.exports = {
     })
   },
 
-  markHelpful: (req, res) => {
-    let config = {
-      headers: {
-        'Authorization': TOKEN
-      }
-    }
-
+  markAnswerAsHelpful: (req, res) => {
   var answer_id = req.params.answer_id
-  console.log(answer_id)
-
   axios.put(`${API_URL}/qa/answers/${answer_id}/helpful`, {}, config)
   .then(() => {
     console.log('Successfully marked answer as helpful.')
@@ -60,6 +45,19 @@ module.exports = {
   })
   .catch(err => {
     console.error('Unsuccessfully marked answer as helpful.')
+    res.status(400).send(err)
+  })
+},
+
+markQuestionAsHelpful: (req, res) => {
+  var question_id = req.params.question_id
+  axios.put(`${API_URL}/qa/questions/${question_id}/helpful`, {}, config)
+  .then(() => {
+    console.log('Successfully marked question as helpful.')
+    res.status(204).send()
+  })
+  .catch(err => {
+    console.error('Unsuccessfully marked question as helpful.')
     res.status(400).send(err)
   })
 }
--- server/routers/qaRouter.js
+++ server/routers/qaRouter.js
@@ -5,9 +5,9 @@ router.get('/questions/:product_id', controller.getAllQuestions);
 router.get('/questions/:question_id/answers', controller.getAllAnswers);
 router.post('/questions');
 router.post('/questions/:question_id/answers');
-router.put('/questions/:question_id/helpful');
+router.put('/questions/:question_id/helpful', controller.markQuestionAsHelpful);
 router.put('/questions/:question_id/report');
-router.put('/answers/:answer_id/helpful', controller.markHelpful);
+router.put('/answers/:answer_id/helpful', controller.markAnswerAsHelpful);
 router.put('/answers/:answer_id/report');
 
 module.exports = router;
