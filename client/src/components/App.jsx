26cb5e1ce0876a52db413a89e2cc92c5edb6a821
Phi Truong <phihtruong00@gmail.com>
2021-11-04 17:28:23 -0700

    Updated App component to hold current product id in state and pass it down to each module

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 import Overview from './overview/overview.jsx';
 import Qa from './qa/qa.jsx';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
 import RelatedProducts from './relatedProducts/relatedProducts.jsx';
 
-const App = () => (
-  <div>
-    <h1>Project Catwalk</h1>
-    <div>
-      <Overview />
-    </div>
-    <RelatedProducts />
-    <Qa />
-    <RatingsAndReviews />
-  </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentProduct: 42366
+    };
 
-)
+    this.handleChangeProduct = this.handleChangeProduct.bind(this);
+  }
+
+  handleChangeProduct(productId) {
+    this.setState({
+      currentProduct: productId
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Project Catwalk</h1>
+        <div>
+          <Overview currentProduct={this.state.currentProduct}/>
+        </div>
+        <RelatedProducts currentProduct={this.state.currentProduct}/>
+        <Qa currentProduct={this.state.currentProduct}/>
+        <RatingsAndReviews currentProduct={this.state.currentProduct}/>
+      </div>
+    )
+  }
+
+}
 export default App;
