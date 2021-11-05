406ed208ae477baf81e0c7bdfed0b88434b6b8dc
Khristian Lopez <khristianlopez@Khristians-MacBook-Air.local>
2021-11-04 23:02:18 -0700

    Converted QA main to Bootstrap

--- client/src/components/qa/qa.jsx
+++ client/src/components/qa/qa.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import QuestionList from './questionlist.jsx'
 import Search from './questionsearch.jsx'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 const Qa = () => (
-  <div className='qa-Main'>
+/*   <div className='qa-Main'>
     <div>Questions and Answers</div>
     <Search />
     <QuestionList/>
-  </div>
+  </div> */
+  <Container fluid="md" className = 'qa-Main'>
+  <Row>
+    <Col>Questions and Answers</Col>
+  </Row>
+  <Row>
+    <Search />
+  </Row>
+  <Row>
+    <QuestionList />
+  </Row>
+</Container>
 )
 
 export default Qa;
