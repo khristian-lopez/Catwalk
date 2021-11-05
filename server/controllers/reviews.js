fc5044aed54236532c47c9fda430507bd1de5dc8
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-11-04 21:40:10 -0700

    implementing API calls for ratings and reviews

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -1,6 +1,18 @@
-module.exports = {
-  getAll: () => {
+const TOKEN = require('../../config.js').TOKEN;
+const axios = require('axios');
+const API_URL = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax`;
 
+module.exports = {
+  getAll: (req, res) => {
+    let config = {
+      headers: {
+        'authorization': TOKEN
+      }
+    }
+    axios.get(`${API_URL}/reviews/`, config)
+      .then(results => {
+        console.log('')
+      })
   },
 
   getOne: () => {
