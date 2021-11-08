3617d9262a8457778248daecbbeb4b8fe9976189
Phi Truong <phihtruong00@gmail.com>
2021-11-08 10:04:43 -0800

    Implemented adding of current product to outfit list; TODO: dynamically render product card info based on current product data

--- client/src/components/relatedProducts/ActionButton.jsx
+++ client/src/components/relatedProducts/ActionButton.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const ActionButton = ({ card }) => (
-  <span className='action-button'>
-    { card === 'outfit' ? '\u24e7' : '\u2606'}
-  </span>
-)
+const ActionButton = ({ card }) => {
+
+  const handleRemove = (e) => {
+    e.persist();
+    console.log(e);
+  }
+
+  return (
+    <span className='action-button' onClick={e => handleRemove(e)}>
+      { card === 'outfit' ? '\u24e7' : '\u2606'}
+    </span>
+  )
+}
 
 ActionButton.propTypes = {
   card: PropTypes.string.isRequired
--- client/src/components/relatedProducts/YourOutfitList.jsx
+++ client/src/components/relatedProducts/YourOutfitList.jsx
@@ -1,29 +1,75 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import AddOutfitCard from './AddOutfitCard.jsx';
 import RelatedProductCard from './RelatedProductCard.jsx';
+import ActionButton from './ActionButton.jsx';
+import ProductInfo from './ProductInfo.jsx';
 
-const YourOutfitList = () => (
-  <CarouselProvider
-  className="outfit-list"
-  naturalSlideWidth={200}
-  naturalSlideHeight={300}
-  totalSlides={2}
-  visibleSlides={4}
-  >
-    <div data-testid='outfit'>
-      <Slider>
-        {/* <Slide index={0}>
-          <RelatedProductCard card={'outfit'}/>
-        </Slide> */}
-        <Slide index={0}>
-          <AddOutfitCard />
-        </Slide>
-      </Slider>
-      <ButtonBack>{'<'}</ButtonBack>
-      <ButtonNext>{'>'}</ButtonNext>
-    </div>
-  </CarouselProvider>
-)
+const YourOutfitList = ({ currentProduct }) => {
+  const [outfit, setOutfit] = useState([]);
+
+  useEffect(() => {
+    let outfitList = JSON.parse(localStorage.getItem('outfit'));
+    if (outfitList) {
+      setOutfit(outfitList);
+    }
+  }, [])
+
+  const handleAdd = () => {
+    if (localStorage.getItem('outfit') === null) {
+      localStorage.setItem('outfit', JSON.stringify([]));
+    }
+
+    let outfitList = JSON.parse(localStorage.getItem('outfit'));
+
+    let checkDupes = outfit.find(product => product.product_id === currentProduct.product_id)
+
+    if (!checkDupes) {
+      outfitList.push(currentProduct);
+      localStorage.setItem('outfit', JSON.stringify(outfitList));
+      outfitList = JSON.parse(localStorage.getItem('outfit'));
+      if (outfitList) {
+        setOutfit(outfitList);
+      }
+    }
+  }
+
+  return (
+    <CarouselProvider
+    className="outfit-list"
+    naturalSlideWidth={200}
+    naturalSlideHeight={300}
+    totalSlides={outfit.length + 1}
+    visibleSlides={4}
+    >
+      <div data-testid='outfit'>
+        <Slider>
+          {outfit.map((product, i) => (
+            <Slide key={product.product_id} index={i}>
+              <div className="cards rel-prod-card">
+                <img className="preview" src={'assets/product-image-placeholder-300x300.jpeg'} />
+                <span>
+                  <ActionButton card={ 'outfit' } />
+                </span>
+                {/* <ProductInfo product={product} /> */}
+              </div>
+            </Slide>
+          ))}
+          <Slide index={outfit.length} onClick={handleAdd}>
+            <AddOutfitCard />
+          </Slide>
+        </Slider>
+        <ButtonBack>{'<'}</ButtonBack>
+        <ButtonNext>{'>'}</ButtonNext>
+      </div>
+    </CarouselProvider>
+  )
+}
+
+YourOutfitList.propTypes = {
+  currentProduct: PropTypes.object
+};
 
 export default YourOutfitList;
