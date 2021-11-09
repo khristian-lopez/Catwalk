ccb5f758f6dfc82c0826cb3fb21fa75afb4d968e
Phi Truong <phihtruong00@gmail.com>
2021-11-09 15:26:48 -0800

    Added tests for ActionButton

--- __tests__/relatedProducts/ActionButton.test.jsx
+++ __tests__/relatedProducts/ActionButton.test.jsx
@@ -10,17 +10,22 @@ import {render, getByText, screen, getByTestId} from '@testing-library/react';
 // add custom jest matchers from jest-dom
 import '@testing-library/jest-dom';
 // the component to test
-import RelatedProducts from '../../client/src/components/RelatedProducts/RelatedProducts.jsx';
+import ActionButton from '../../client/src/components/RelatedProducts/ActionButton.jsx';
 
-xdescribe('related ActionButton component', () => {
-  test('renders a related products list and an outfit list', () => {
-    render(<RelatedProducts />);
+describe('ActionButton component', () => {
+  test('renders a star icon if in the related products list', () => {
+    render(<ActionButton card={'related'} />);
 
-    const ancestor = screen.getByTestId('related-products');
-    const descendant = screen.getByTestId('related-prod-list');
-    const descendant2 = screen.getByTestId('outfit');
+    const button = screen.getByTestId('action-button');
 
-    expect(ancestor).toContainElement(descendant);
-    expect(ancestor).toContainElement(descendant2);
+    expect(button).toHaveTextContent('\u2B50');
+  })
+
+  test('renders a "X" icon if in the outfit list', () => {
+    render(<ActionButton card={'outfit'} />);
+
+    const button = screen.getByTestId('action-button');
+
+    expect(button).toHaveTextContent('\u274c');
   })
 })
--- client/src/components/RelatedProducts/ActionButton.jsx
+++ client/src/components/RelatedProducts/ActionButton.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ActionButton = ({ card, productId, handleRemove, openModal, product }) => {
 
   return (
-    <span className='action-button' onClick={card === 'outfit' ? () => handleRemove(productId) : (e) => openModal(e)}>
+    <span className='action-button' data-testid='action-button'onClick={card === 'outfit' ? () => handleRemove(productId) : (e) => openModal(e)}>
       { card === 'outfit' ? '\u274c' : 	'\u2B50'}
     </span>
   )
