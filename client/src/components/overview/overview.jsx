4d2e3e5db0e2059a98a7393e5aa73c32efe1f28c
Mike Ortiz <emike108@aim.com>
2021-11-08 16:18:49 -0800

    selected style renders after being clicke

--- client/src/components/overview/overview.jsx
+++ client/src/components/overview/overview.jsx
@@ -8,17 +8,14 @@ import Col from 'react-bootstrap/Col'
 import axios from 'axios';
 import defaultData from './defaultData.js';
 
-const Overview = ({ currentProduct, currentStyle }) => {
+const Overview = ({ currentProduct, currentStyle, handleChangeStyle }) => {
   // create a useState that obtains current product info
   const [productInfo, changeProductInfo]  = useState([])
   // create a useState that obtains current style info
   const [styleInfo, changeStyleInfo] = useState([])
-  // console.log('style object', currentProduct.defaultStyle.style_id)
-  console.log('style info', styleInfo)
 
   //create a useEffect to retreive the product info for currentProduct
   useEffect(() => {
-    // console.log('at overview', currentProduct)
     axios.get(`/products/${currentProduct}`)
     .then(results => changeProductInfo(results.data))
     .catch(err => console.error('Not able to obtain product info', err))
@@ -26,21 +23,16 @@ const Overview = ({ currentProduct, currentStyle }) => {
 
   // create a useEffect to retreive the style info the is selected
   useEffect(() => {
-    // console.log('at styles effect', currentProduct)
     axios.get(`/products/${currentProduct}/styles`)
     .then(results => {
       for (var style of results.data) {
         if (style.style_id === currentStyle) {
           changeStyleInfo(style)
-          //onlyneed to send style as an object since only one being sent and thus don't need to map over different styles
-          // console.log('a', style)
-          // console.log('b', style.style_id)
-
         }
       }
     })
     .catch(err => console.error('Not able to obtain style info', err))
-  }, [currentProduct])
+  }, [currentProduct, currentStyle])
     //needs to be able to change style when product is changed (to default style) and change when a different product is chosen.
 
     return (
@@ -49,7 +41,7 @@ const Overview = ({ currentProduct, currentStyle }) => {
           <h1>Overview</h1>
         </Row>
         <Row>
-          <RowOne productInfo={productInfo} styleInfo={styleInfo}/>
+          <RowOne productInfo={productInfo} styleInfo={styleInfo} handleChangeStyle={handleChangeStyle}/>
         </Row>
         <Row>
           <RowTwo productInfo={productInfo}/>
