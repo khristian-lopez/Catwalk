7cebc1c93070b9611bf44741e31cec8513d03ab3
Phi Truong <phihtruong00@gmail.com>
2021-11-08 22:47:25 -0800

    Created skeleton of comparisons table in modal

--- client/src/components/relatedProducts/RelatedProductCard.jsx
+++ client/src/components/relatedProducts/RelatedProductCard.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Slide } from 'pure-react-carousel';
 import Modal from 'react-bootstrap/Modal';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import ActionButton from './ActionButton.jsx';
 import ProductInfo from './ProductInfo.jsx';
 import PropTypes from 'prop-types';
@@ -20,15 +23,37 @@ const RelatedProductCard = (props) => {
       <div className="cards rel-prod-card" onClick={(() => props.handleChangeProduct(props.product.id))}>
         <img className="preview" src={props.product.defaultStyle.photos[0].thumbnail_url} />
         <span>
-          <ActionButton card={props.card} openModal={handleShow}/>
+          <ActionButton card={props.card} product={props.product} openModal={handleShow}/>
         </span>
         <ProductInfo product={props.product}/>
       </div>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title as='h6'>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body className="show-grid">
+          <Container className="modal-container">
+            <Row className="modal-row-1">
+              <Col xs={6}>
+                Product Short Name
+              </Col>
+              <Col xs={6}>
+                Product Short Name
+              </Col>
+            </Row>
+            <Row className="modal-row-2">
+              <Col xs={2} className="modal-col-1">
+                .col-xs-3
+              </Col>
+              <Col xs={8} className="modal-col-2">
+                .col-xs-6
+              </Col>
+              <Col xs={2} className="modal-col-3">
+                .col-xs-3
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
       </Modal>
     </Slide>
   )
