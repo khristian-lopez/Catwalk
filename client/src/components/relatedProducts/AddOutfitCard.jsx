a934c69997b304fbddd3b4494e1916bf8e2a6adc
Phi Truong <phihtruong00@gmail.com>
2021-11-09 12:08:03 -0800

    Added a test for YourOutfitList

--- client/src/components/relatedProducts/AddOutfitCard.jsx
+++ client/src/components/relatedProducts/AddOutfitCard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ActionButton from './ActionButton.jsx';
 
 const AddOutfitCard = () => (
-  <div className='cards add-outfit-card'>
+  <div className='cards add-outfit-card' data-testid='add-outfit'>
     <h6 className='add-outfit'> &#8853; <br/> Add Current <br/> Product to Outfit </h6>
   </div>
 )
