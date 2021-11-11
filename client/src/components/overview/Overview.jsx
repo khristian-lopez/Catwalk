923c88040539716e217831a73b3879f1adb7e608
Mike Ortiz <emike108@aim.com>
2021-11-11 10:16:04 -0800

    attempting to resolve merge conflict

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -9,19 +9,15 @@ import axios from 'axios';
 import defaultData from './defaultData.js';
 
 const Overview = ({ currentProduct, currentStyle, handleChangeStyle, postUserClick }) => {
-  // create a useState that obtains current product info
   const [productInfo, changeProductInfo]  = useState([])
-  // create a useState that obtains current style info
   const [styleInfo, changeStyleInfo] = useState([])
 
-  //create a useEffect to retreive the product info for currentProduct
   useEffect(() => {
     axios.get(`/products/${currentProduct}`)
     .then(results => changeProductInfo(results.data))
     .catch(err => console.error('Not able to obtain product info at overview product', err))
   }, [currentProduct])
 
-  // create a useEffect to retreive the style info the is selected
   useEffect(() => {
     axios.get(`/products/${currentProduct}/styles`)
     .then(results => {
@@ -33,7 +29,6 @@ const Overview = ({ currentProduct, currentStyle, handleChangeStyle, postUserCli
     })
     .catch(err => console.error('Not able to obtain style info at overview style', err))
   }, [currentProduct, currentStyle])
-    //needs to be able to change style when product is changed (to default style) and change when a different product is chosen.
 
     return (
       <Container fluid="md" className="ov-main" data-testid="ov-main" onClick={(e) => postUserClick(e, 'Overview')}>
