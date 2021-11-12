da85297a92123745d88188b092cb5afed035fda6
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-11-12 13:44:03 -0800

    working on rendering stars onto modal form rating

--- client/src/components/RatingsAndReviews/Reviews/ModalForm.jsx
+++ client/src/components/RatingsAndReviews/Reviews/ModalForm.jsx
@@ -65,20 +65,20 @@ const ModalForm = ({closeModal, productId}) => {
             setImage(url);
         }
     }
-
+    //console.log(typeof product.id)
     // handleFormSubmit
     const handleFormSubmit = () => {
         let uploadedImages = [image, ...selectedImages]
         const input = {
-            product_id: product.id,
-            rating: Number(rateValue),
-            summary: summary,
-            body: review,
-            recommend: (recValue === "true"),
-            name: name,
-            email: email,
-            photos: uploadedImages,
-            characteristics: {
+            "product_id": product.id,
+            "rating": Number(rateValue),
+            "summary": summary,
+            "body": review,
+            "recommend": (recValue === "true"),
+            "name": name,
+            "email": email,
+            "photos": uploadedImages,
+            "characteristics": {
                 "size": size,
                 "width": width,
                 "comfort": comfort,
@@ -89,12 +89,12 @@ const ModalForm = ({closeModal, productId}) => {
         }
         addReview(input)
     }
-    
+    // 'multipart/form-data'
     // TODO: create axios post request to add review
     const addReview = data => {
         return axios.post(`/reviews/${product.id}`, data, {
             header: {
-                'Content-type': 'multipart/form-data'
+                'Content-type': 'application/json'
             }
         })
         .then(res => {
@@ -109,26 +109,26 @@ const ModalForm = ({closeModal, productId}) => {
         <div>
             <Form className="modalContainer" onSubmit={(e) => {
                 e.preventDefault()
-                closeModal(false)
                 handleFormSubmit()
+                closeModal(false)
             }}>
                 <div className="form-input">
                     <Form.Group>
                         <Form.Label>Overall Rating</Form.Label>
                             <br></br>
-                            <Form.Check
+                            <Form.Check id="rev-overall-rate"
                                 inline label="Poor" value="1" {...rateInputProps} checked={rateValue === "1"}
                             />
-                            <Form.Check
+                            <Form.Check id="rev-overall-rate"
                                 inline label="Fair" value="2" {...rateInputProps} checked={rateValue === "2"}
                             />
-                            <Form.Check
+                            <Form.Check id="rev-overall-rate"
                                 inline label="Average" value="3" {...rateInputProps} checked={rateValue === "3"}
                             />
-                            <Form.Check
+                            <Form.Check id="rev-overall-rate"
                                 inline label="Good" value="4" {...rateInputProps} checked={rateValue === "4"}
                             />
-                            <Form.Check
+                            <Form.Check id="rev-overall-rate"
                                 inline label="Great" value="5" {...rateInputProps} checked={rateValue === "5"}
                             />
                     </Form.Group>
@@ -286,77 +286,3 @@ const ModalForm = ({closeModal, productId}) => {
 }
 
 export default ModalForm;
-
-
-{/* <div className="review-overall">Overall rating:
-    <p id="review-choices">
-        <input className="review-rate" value="1" {...rateInputProps} checked={rateValue === "1"} />Poor
-        <input className="review-rate" value="2" {...rateInputProps} checked={rateValue === "2"} />Fair
-        <input className="review-rate" value="3" {...rateInputProps} checked={rateValue === "3"} />Average
-        <input className="review-rate" value="4" {...rateInputProps} checked={rateValue === "4"} />Good
-        <input className="review-rate" value="5" {...rateInputProps} checked={rateValue === "5"} />Great
-    </p>
-</div> */}
-{/* <div className="review-recc" required>Do you recommend this product?
-    <p id="recc-answer" >
-        <input id="recc-yes" value="true" type="radio"  {...recInputProps} checked={recValue === "true"} />Yes
-        <input id="recc-no" value="false" type="radio" {...recInputProps} checked={recValue === "false"} />No
-    </p>
-</div> */}
-{/* <div className="review-photos">Add photo: (max 5)
-    <br></br>
-    <input
-        type="file"
-        name="image"
-        accept="image/*"
-        onChange={handleSelectImage}
-        multiple
-    />
-<div>
-    <img src={image} style={{ margin: "10px", width: "200px", height: "200px" }}/>
-    <img src={selectedImages[0]} style={{ margin: "10px", width: "200px", height: "200px" }} />
-    <img src={selectedImages[1]} style={{ margin: "10px", width: "200px", height: "200px" }} />
-    <img src={selectedImages[2]} style={{ margin: "10px", width: "200px", height: "200px" }} />
-    <img src={selectedImages[3]} style={{ margin: "10px", width: "200px", height: "200px" }} />
-</div>
-</div> */}
-{/* <Characteristics handleChar={handleChar}/> */}
-{/* <div className="review-summary">Summary:
-    <br></br>
-    <textarea 
-        type="text" 
-        cols="50" 
-        placeholder="Ex: Best purchase ever!" 
-        maxLength="60"
-        onChange={(e) => setSummary(e.target.value)}
-        
-    />
-</div> */}
-{/* <div className="review-body">Review: */}
-{/* <br></br> */}
-{/* <textarea
-    onChange={(e) => {
-        setReview(e.target.value)
-        setCharacterCount(e.target.value.length)
-    }}
-    type="text" rows="5" cols="50"
-    placeholder="Why did you like the product or not?"
-    minLength="50" maxLength="1000" required
-/> */} {/* </div> */}
-{/* <div className="reviewer-name">username:
-<br></br>
-<input 
-    onChange={(e) => setName(e.target.value)}
-    type="text" 
-    placeholder="Ex: jackson11!" 
-    maxLength="60" required
-/>
-<p style={{ fontSize: "10px" }}>For privacy reasons, do not use your full name or email address</p>
-</div> */}
-{/* <button id="review-submit-btn" type="submit" 
-        onSubmit={e => {
-            e.preventDefault()
-            closeModal(false)
-            }
-        }
->Submit your review!</button> */}
